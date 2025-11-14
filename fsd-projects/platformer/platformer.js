bbd08b99f362698a127b8ff32cadc3a2448aa8a5
SophieFruge7339844 <saf7339844@lpssonline.com>
2025-11-14 13:32:39 +0000

    todo 2 complete

--- fsd-projects/platformer/platformer.js
+++ fsd-projects/platformer/platformer.js
@@ -40,15 +40,18 @@ createPlatform(340,540,200,13);
 
 
     // TODO 3 - Create Collectables
-createCollectable("max", 500, 550);
-createCollectable("kennedi",700, 500);
-createCollectable("steve",500,450 );
+createCollectable('max', 500, 550);
+createCollectable('kennedi',700, 500);
+createCollectable('steve',500,450 );
 
 
 
     
     // TODO 4 - Create Cannons
-     createCannon(“right”, 200, 750); 
+     createCannon('right',250,300); 
+     createCannon('top', 600, 700)
+     createCannon('bottom', 900, 700)
+        //createCannon('bottom',850,700);
 
 
     
